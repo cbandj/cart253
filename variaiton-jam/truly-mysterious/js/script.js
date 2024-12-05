99c2569183f924eef1ae2b0e0926542242e051db
xiliyo <151044882+xiliyo@users.noreply.github.com>
2024-12-05 02:44:41 -0500

    barebones for states

--- variaiton-jam/truly-mysterious/js/script.js
+++ variaiton-jam/truly-mysterious/js/script.js
@@ -1,24 +1,89 @@
 /**
- * Title of Project
- * Author Name
+ * Truly Mysterious!
+ * Carael Bandojo
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * A point and click escape room, 
+ * Find the code to escape the room!
+ * If you guess wrong thrice, or fail to escape when the timer ends, you die!
+ * 
+ * Instructions:
+ * - Click to interact with objects
+ * 
+ * Uses:
+ * p5.js
+ * https://p5js.org
  */
 
 "use strict";
 
+/*VARIABLES---------------------------------------------------------*/ 
+
+// States for each screen of the game
+const States = {
+    TITLE: "title",
+    ROOM: "room",
+    DOOR: "door",
+    WIN: "win",
+    LOSE: "lose"
+  };
+  
+
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+* Make canvas and set background
 */
 function setup() {
-
+createCanvas(1000,700);
+background(200);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Sense what state the game is in and draw it
 */
 function draw() {
+ // Title state
+    if (state === States.TITLE) {
+        title();
+    } // Room state
+    else if (state === States.ROOM) {
+        room();
+    } // Door State
+    else if (state === States.DOOR) {
+        door();
+    } // Win State
+    else if (state === States.WIN) {
+        win();
+    } // Door State
+    else if (state === States.LOSE) {
+        lose();
+    } 
+}
 
+
+/**
+* Draw title state.
+*/
+function title(){
+  
+}
+
+/**
+* Draw room state.
+*/
+function room(){
+  
+}
+
+/**
+* Draw door state.
+*/
+function door(){
+  
+}
+
+/**
+* Draw win state.
+*/
+function lose(){
+  
 }
